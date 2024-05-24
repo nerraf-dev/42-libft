a5573ce4d779ac477635227ab3e87b718d0510f8
Simon Farren <sfarren@c1r2s1.42malaga.com>
2024-05-24 14:40:31 +0200

    campus

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: sfarren <sfarren@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 14:10:20 by sfarren           #+#    #+#             */
-/*   Updated: 2024/04/29 14:11:12 by sfarren          ###   ########.fr       */
+/*   Updated: 2024/05/24 13:38:53 by sfarren          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,4 @@
 void ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
     // Implementation goes here
-}
-{
-
 }
--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -6,10 +6,34 @@
 /*   By: sfarren <sfarren@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 14:09:08 by sfarren           #+#    #+#             */
-/*   Updated: 2024/04/29 14:11:14 by sfarren          ###   ########.fr       */
+/*   Updated: 2024/05/24 14:27:36 by sfarren          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-//TODO: ft_strmapi
+/*
+params
+ s: The string on which to iterate.
+ f: The function to apply to each character.
 
+return:
+ The string created from the successive applications
+ of ’f’.
+ Returns NULL if the allocation fails.
+
+Desc:
+ Applies the function f to each character of the
+ string s, passing its index as the first argument
+ and the character itself as the second. A new
+ string is created (using malloc(3)) to collect the
+ results from the successive applications of f.
+*/
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	// get s len
+	// allocate mem (malloc) for new str
+	// if malloc fails retun 0
+	// iterate over string, applying func 'f'
+	// write f return value to new string
+	//return new string
+}
