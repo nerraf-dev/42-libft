1febfb4f8b607d16d0a124defb600356c32c49e6
nerraf-dev <87069314+nerraf-dev@users.noreply.github.com>
2024-12-29 17:54:36 +0100

    Add ft_printf_fd function and related updates for file descriptor output

--- src/ft_printf/ft_printchr.c
+++ src/ft_printf/ft_printchr.c
@@ -6,7 +6,7 @@
 /*   By: sfarren <sfarren@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 14:18:46 by sfarren           #+#    #+#             */
-/*   Updated: 2024/07/17 09:49:52 by sfarren          ###   ########.fr       */
+/*   Updated: 2024/12/29 17:42:56 by sfarren          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,16 @@ void	ft_printchr(char c, int *count)
 	if (*count == -1)
 		return ;
 	if (write(1, &c, 1) == -1)
+		*count = -1;
+	else
+		*count += 1;
+}
+
+void	ft_printchr_fd(char c, int *count, int fd)
+{
+	if (*count == -1)
+		return ;
+	if (write(fd, &c, 1) == -1)
 		*count = -1;
 	else
 		*count += 1;
