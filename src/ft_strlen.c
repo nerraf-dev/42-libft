1b803382707d3d7183066627d32e8eef1cc7b492
nerraf-dev <87069314+nerraf-dev@users.noreply.github.com>
2025-04-18 19:57:35 +0200

    Update ft_strlen.c documentation for clarity and accuracy

--- src/ft_strlen.c
+++ src/ft_strlen.c
@@ -6,12 +6,23 @@
 /*   By: sfarren <sfarren@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 21:58:38 by sfarren           #+#    #+#             */
-/*   Updated: 2024/05/15 14:05:20 by sfarren          ###   ########.fr       */
+/*   Updated: 2025/04/18 18:45:03 by sfarren          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * @file ft_strlen.c
+ * @brief Implementation of the ft_strlen function.
+ *
+ * This function calculates the length of a null-terminated string.
+ *
+ * @param str The input string whose length is to be calculated.
+ *            It must be a valid null-terminated string.
+ *
+ * @return The length of the string (no. of characters before \0).
+ */
 size_t	ft_strlen(const char *str)
 {
 	size_t	i;
